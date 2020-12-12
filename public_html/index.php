04bbc83d733c063b683b5e3d7dccaee1b6b5e8d0
sergei19860511 <sergei_kupriynov@mail.ru>
2020-12-12 18:10:52 +0400

    update index

--- public_html/index.php
+++ public_html/index.php
@@ -7,5 +7,12 @@ require 'Picture.php';
 	if (!isset($_GET['page'])) {
 		include 'templates/allPicture.php';
 	}elseif ($_GET['page'] == 'onePicture') {
+		$picture = [];
+		foreach (Picture::getTable($DB) as $pictures) {
+			if($pictures['id_good'] == $_GET['id_good']){
+				$picture = $pictures;
+				break;
+			}
+		}
 		include 'templates/onePicture.php';
 	}
